3605fccdd3b20fc5fac9a5d3e5b96f4e2bde1bd5
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2021-08-17 23:37:40 +0200

    PostCSS config and TailwindTheme
    Made postcss config shorter and fixed warning in TailwindTheme about checking if an item in local storage is set.

--- src/components/TailwindTheme.svelte
+++ src/components/TailwindTheme.svelte
@@ -17,7 +17,7 @@
     onMount(() => {
         if (
             localStorage.theme === "dark" ||
-            (!"theme" in localStorage &&
+            (localStorage.getItem("theme") !== null &&
                 window.matchMedia("(prefers-color-scheme: dark)").matches)
         ) {
             document.querySelector("html").classList.add("dark");
